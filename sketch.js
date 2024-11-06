b39e551a8d02a34b60f7831aabadaa8bbe916dc5
Afrodite <106694662+vinicinho30@users.noreply.github.com>
2024-11-06 15:21:56 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 function setup() {
-  createCanvas(2020, 2080, WEBGL); //plano 3D
+  createCanvas(1920, 1080, WEBGL); //plano 3D
    terra=loadImage('terra.jpg')
 }
 
 function draw() {
   background(130, 100, 84); //cor de fundo
-  rotateY(millis()/1000); // rotação da terra
+  rotateY(millis()/500); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(260, 120); //esfera 3D
+  sphere(250, 250); //esfera 3D
   
   
   
