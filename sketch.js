637d6ae90e4e029e68b1f81d53e01928838c6246
Afrodite <106694662+vinicinho30@users.noreply.github.com>
2024-11-06 14:49:55 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function draw() {
   background(130, 100, 84); //cor de fundo
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(700, 300); //esfera 3D
+  sphere(260, 120); //esfera 3D
   
   
   
